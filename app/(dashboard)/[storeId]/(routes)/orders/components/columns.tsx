16ebc117be6740cae8aae1e1347e4bb9c579d755
aderusmana <aderusmana777@gmail.com>
2024-01-09 17:10:32 +0700

    fix ispaid column

--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -43,7 +43,17 @@ export const columns: ColumnDef<OrderColumn>[] = [
   },
   {
     accessorKey: 'isPaid',
-    header: "Paid"
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Is Paid
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: 'createdAt',
