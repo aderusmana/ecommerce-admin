6b699300471f24334f367518e38dd99181f3762a
aderusmana <aderusmana777@gmail.com>
2024-01-09 18:05:29 +0700

    root fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,10 @@ export default function SetupPage() {
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
 
-
+  useEffect(() => {
+      onOpen();
+    
+  }, [isOpen, onOpen]);
 
   return <div className={'p-4'}>Root Page</div>;
 }
